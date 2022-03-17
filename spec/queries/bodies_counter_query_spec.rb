141c19b54be446a9802cad907cfbcc2b68f34047
Felipe <fe.peressim@gmail.com>
2022-03-17 10:07:43 -0300

    Task 1: adding suggested improvements to bodies_counter_query_spec from review

--- spec/queries/bodies_counter_query_spec.rb
+++ spec/queries/bodies_counter_query_spec.rb
@@ -3,56 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe BodiesCounterQuery do
-  let(:star1) do
-    Star.create!(
-      name: 'Proxima Centauri',
-      mass: 2.446,
-      distance_from_earth: 4.246
-    )
-  end
-
-  let(:star2) do
-    Star.create!(
-      name: 'Alpha Centauri 333',
-      mass: 2.188,
-      distance_from_earth: 4.367
-    )
-  end
-
-  let(:planet) do
-    Planet.create!(
-      name: 'Earth',
-      mass: 1.0,
-      distance_from_star: 4.367,
-      orbital_period: 365,
-      tidally_locked: false,
-      category: :rocky,
-      star_id: star1.id
-    )
-  end
-
-  let(:bodies_counter) do
-    described_class.new
-  end
+  subject(:bodies_counter) { described_class.new }
 
   describe '#count_stars' do
+    subject(:count_stars) { bodies_counter.count_stars }
+
     before do
-      star1
-      star2
+      Star.create!(name: 'Alpha Centauri 333', mass: 2.188, distance_from_earth: 4.367)
+      Star.create!(name: 'Proxima Centauri', mass: 2.446, distance_from_earth: 4.246)
     end
 
     it 'counts the number of stars' do
-      expect(bodies_counter.count_stars).to eq(2)
+      expect(count_stars).to eq(2)
     end
   end
 
   describe '#count_planets' do
+    subject(:count_planets) { bodies_counter.count_planets }
+
     before do
-      planet
+      Star.create!(name: 'Alpha Centauri 333', mass: 2.188, distance_from_earth: 4.367)
+      Planet.create!(name: 'Earth', mass: 1.0, distance_from_star: 4.367, orbital_period: 365, tidally_locked: false,
+                     category: :rocky, star_id: Star.first.id)
     end
 
     it 'counts the number of planets' do
-      expect(bodies_counter.count_planets).to eq(1)
+      expect(count_planets).to eq(1)
     end
   end
 
