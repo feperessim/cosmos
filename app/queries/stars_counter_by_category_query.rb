64ef1249698f1046cb969fb581cabc503893ceb2
Felipe <fe.peressim@gmail.com>
2022-03-17 13:26:09 -0300

    Task 2: adding suggested improvements to stars_counter_by_category_query_spec from review.

--- app/queries/stars_counter_by_category_query.rb
+++ app/queries/stars_counter_by_category_query.rb
@@ -1,4 +1,4 @@
-class StarsCounterByCategoryQuery 
+class StarsCounterByCategoryQuery
   def count(category)
     @count_category = @count_category || Star.group(:category).count(:name)
     @count_category[category] || 0
--- spec/queries/stars_counter_by_category_query_spec.rb
+++ spec/queries/stars_counter_by_category_query_spec.rb
@@ -3,67 +3,42 @@
 require 'rails_helper'
 
 RSpec.describe StarsCounterByCategoryQuery do
-  before do
-    Star.create!(
-      name: 'Proxima Centauri',
-      mass: 2.446,
-      category: :O,
-      distance_from_earth: 4.246
-    )
+     context 'When we have 2 stars of O' do 
+      before do
+        Star.create!(
+          name: 'Proxima Centauri',
+          mass: 2.446,
+          category: :O,
+          distance_from_earth: 4.246
+        )
 
-    Star.create!(
-      name: 'Alpha Centauri 333',
-      mass: 2.188,
-      category: :O,
-      distance_from_earth: 4.367
-    )
+        Star.create!(
+          name: 'Alpha Centauri 333',
+          mass: 2.188,
+          category: :O,
+          distance_from_earth: 4.367
+        )
+      end
 
-    Star.create!(
-      name: 'Star 1',
-      mass: 27_345_242.82971121,
-      category: :B,
-      distance_from_earth: 32.26483727370002
-    )
+      describe '#count' do
+        subject(:category_count) { described_class.new.count(category) }
+        
+        context 'When category is O' do
+          let(:category) { 'O' }
+          it { expect(subject).to eq(2) }
+          
+        end
 
-    Star.create!(
-      name: 'Star 2',
-      mass: 50_045_831.75365358,
-      category: :A,
-      distance_from_earth: 11.81970786818194
-    )
-  end
+        context 'When category is A' do
+          let(:category) { 'A' }
+          it { expect(subject).to eq(0) } 
+        end
 
-  let(:stars_counter_by_category) do
-    described_class.new
-  end
-
-  let(:stars_count_by_cat_hash) do
-    {
-      'O' => stars_counter_by_category.count('O'),
-      'B' => stars_counter_by_category.count('B'),
-      'A' => stars_counter_by_category.count('A'),
-      'F' => stars_counter_by_category.count('F'),
-      'G' => stars_counter_by_category.count('G'),
-      'K' => stars_counter_by_category.count('K'),
-      'M' => stars_counter_by_category.count('M')
-    }
-  end
-
-  describe '#count' do
-    it 'counts the number of stars given a category' do
-      expect(
-        stars_count_by_cat_hash
-      ).to include(
-        {
-          'O' => 2,
-          'B' => 1,
-          'A' => 1,
-          'F' => 0,
-          'G' => 0,
-          'K' => 0,
-          'M' => 0
-        }
-      )
-    end
-  end
+        context 'When category is invalid' do
+          let(:category) { nil }
+          it { expect(subject).to eq(0) } 
+        end
+      end
+     end
 end
+
